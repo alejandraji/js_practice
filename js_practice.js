9537d261d92feb3ca899aa1d4cadb9217171b727
Alejandra Jimenez <axjimenez89@gmail.com>
2017-01-14 17:46:15 -0500

    added compare function

--- js_practice.js
+++ js_practice.js
@@ -13,4 +13,9 @@ if (computerChoice<=0.33){
     alert("scissors");
 }
 
+var compare = function(choice1, choice2){
+    if(choice1 === choice2){
+            return "The result is a tie!";
+    }
+};
 
