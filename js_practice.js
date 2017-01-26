8e1281bc145c58e66de72da7564ed0c2edcab17e
Alejandra Jimenez <axjimenez89@gmail.com>
2017-01-26 15:33:38 -0500

    for loops

--- js_practice.js
+++ js_practice.js
@@ -49,3 +49,21 @@ var compare = function (choice1, choice2) {
 
 
 
+/// for loops breakdown
+// Edit this for loop to start at 4 and end at 23
+
+for (var i = 4; i <=23; i = i + 1) {
+  console.log(i);
+}
+
+
+// created a for loop that counts every number 0 to 35, made it start at 5 and stop counting with 50
+// only counts every fifth number so i+=5
+
+for (var i = 5; i <=50; i+=5) {
+  console.log(i);
+}
+
+
+
+
