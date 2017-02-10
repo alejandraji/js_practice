1c558d45bd5ffcf8f8acad9133d22d487d516d74
Alejandra Jimenez <axjimenez89@gmail.com>
2017-02-10 15:47:33 -0500

    practice with switch statements

--- js_practice.js
+++ js_practice.js
@@ -189,3 +189,24 @@ var isEven = function(number) {
      return false;
  }
 };
+
+
+/// using the switch statements
+
+var color = prompt("What's your favorite primary color?","Type your favorite color here");
+
+switch(color) {
+  case 'red':
+    console.log("Red's a good color!");
+    break;
+  case 'blue':
+    console.log("That's my favorite color, too!");
+    break;
+case 'yellow':
+    console.log("I love bright and yellow hats");
+    break;
+  //Add your case here!
+
+  default:
+    console.log("I don't think that's a primary color!");
+}
