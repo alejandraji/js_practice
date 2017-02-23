a2594978eb2f42e41701854d0a7f436b106684ea
Alejandra Jimenez <axjimenez89@gmail.com>
2017-02-23 00:02:56 -0500

    objects

--- js_practice.js
+++ js_practice.js
@@ -492,3 +492,15 @@ var name2 = susan.name;
 var age2 = susan.age;
 
 //these are global variables
+// accessing variables using bracket notation
+// Take a look at our next example object, a dog
+var dog = {
+  species: "greyhound",
+  weight: 60,
+  age: 4
+};
+
+var species = dog["species"];
+// fill in the code to save the weight and age using bracket notation
+var weight = dog["weight"];
+var age = dog["age"];
