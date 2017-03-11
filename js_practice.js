662a0ce2e40b04c85fe880a5807371f07b67211d
Alejandra Jimenez <axjimenez89@gmail.com>
2017-03-11 00:22:18 -0500

    objects

--- js_practice.js
+++ js_practice.js
@@ -594,6 +594,58 @@ var alice = new Person("Alice", 30);
 var billy = new Person("Billy", 25);
 
 console.log("The older person is " + olderAge(alice, billy));
+/// var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
 
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function printPerson(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+
+function list() {
+  var contactsLength = contacts.length;
+  for (var i = 0; i < contactsLength; i++) {
+    printPerson(contacts[i]);
+  }
+}
+
+/*Create a search function
+then call it passing "Jones"*/
+
+
+var search = function(lastName){
+    var contactsLength = contacts.length;
+    for (var i = 0; i < contactsLength; i++) {
+if (contacts[i].lastName === lastName){
+            printPerson(contacts[i]);
+     };
+   };
+}
+search("Jones");
+
+var add = function(firstName,lastName,email,phoneNumber)
+    {
+        contacts[contacts.length] = {
+        firstName: firstName,
+        lastName: lastName,
+        phoneNumber: phoneNumber,
+        email: email
+        }
+    }
+
+add("Alejandra","jimenez","ajimenez@gmail.com","555-5555");
+list();
 
 
