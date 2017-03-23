5d1b63e1849f3d4f390fbd58c5627106442da63f
Alejandra Jimenez <axjimenez89@gmail.com>
2017-03-23 17:45:50 -0400

    prototypes

--- js_practice.js
+++ js_practice.js
@@ -666,3 +666,20 @@ Emperor.prototype = new Penguin ();
 var emperor = new Emperor ( "alejandra" );
 console.log ( emperor.numLegs );
 
+
+//Declare a third variable called myAge and use it to store the age property of the john object.
+
+
+function Person(first,last,age) {
+   this.firstName = first;
+   this.lastName = last;
+   this.age = age;
+}
+
+var john = new Person('John','Smith',30);
+var myFirst = john.firstName;
+var myLast = john.lastName;
+var myAge = john.age;
+//declare variable myAge set to the age of the john object.
+
+
